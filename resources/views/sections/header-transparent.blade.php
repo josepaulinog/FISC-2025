b8127db13b2a8362631ceb7858470f44b5465901
José Paulino <jose.paulino@freebalance.com>
2025-03-17 19:17:42 -0400

    remove sticky

--- resources/views/sections/header-transparent.blade.php
+++ resources/views/sections/header-transparent.blade.php
@@ -1,4 +1,4 @@
-<header class="fixed w-full z-10 bg-transparent">
+<header class="absolute top-0 left-0 right-0 w-full z-10 bg-transparent">
   <div class="container mx-auto py-4 px-4">
     @if (has_nav_menu('primary_navigation'))
     <nav class="navbar px-0" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
