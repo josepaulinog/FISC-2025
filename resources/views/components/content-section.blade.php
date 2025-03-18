39f4cef58508f1e029a0109c2241856bd4058f1e
José Paulino <jose.paulino@freebalance.com>
2025-03-18 15:04:43 -0400

    Update

--- resources/views/components/content-section.blade.php
+++ resources/views/components/content-section.blade.php
@@ -22,7 +22,7 @@
       @if($imagePosition === 'vectorLeft')
         <!-- Illustration on the left -->
         <div class="w-full lg:w-1/2">
-          <img src="{{ $imageUrl }}" alt="{{ $imageAlt }}" class="mx-auto mb-8 lg:mb-0" />
+          <img src="{{ $imageUrl }}" alt="{{ $imageAlt }}" class="mx-auto mb-4 lg:mb-0" />
         </div>
         <!-- Text Content on the right -->
         <div class="w-full lg:w-1/2">
@@ -34,7 +34,7 @@
             {{ $paragraph2 }}
           </p>
           @if (!empty($items))
-            <ul class="space-y-4 text-left text-neutral-600 dark:text-gray-400 mb-8 md:mb-0 ms-4">
+            <ul class="space-y-4 text-left text-neutral-600 dark:text-gray-400 mb-2 md:mb-0 ms-4">
               @foreach ($items as $item)
                 <li class="flex items-center space-x-3 rtl:space-x-reverse">
                 <svg class="shrink-0 w-4 h-4 text-secondary dark:text-secondary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
@@ -59,7 +59,7 @@
           </p>
           @endif
           @if (!empty($items))
-            <ul class="space-y-4 text-left text-neutral-600 dark:text-gray-400 mb-8 md:mb-0 ms-4">
+            <ul class="space-y-4 text-left text-neutral-600 dark:text-gray-400 mb-2 md:mb-0 ms-4">
               @foreach ($items as $item)
                 <li class="flex items-center space-x-3 rtl:space-x-reverse">
                 <svg class="shrink-0 w-4 h-4 text-secondary dark:text-secondary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
@@ -78,7 +78,7 @@
       @elseif($imagePosition === 'left')
         <!-- Illustration on the left -->
         <div class="w-full lg:w-1/2">
-          <img src="{{ $imageUrl }}" alt="{{ $imageAlt }}" class="lg:max-w-md mx-auto rounded-lg shadow-lg mb-8 lg:mb-0" />
+          <img src="{{ $imageUrl }}" alt="{{ $imageAlt }}" class="lg:max-w-md mx-auto rounded-lg shadow-lg mb-2 lg:mb-0" />
         </div>
         <!-- Text Content on the right -->
         <div class="w-full lg:w-1/2">
@@ -92,7 +92,7 @@
           </p>
           @endif
           @if (!empty($items))
-            <ul class="space-y-4 text-left text-neutral-600 dark:text-gray-40 lg:mb-8 ms-4">
+            <ul class="space-y-4 text-left text-neutral-600 dark:text-gray-40 lg:mb-2 ms-4">
               @foreach ($items as $item)
                 <li class="flex items-center space-x-3 rtl:space-x-reverse">
                 <svg class="shrink-0 w-4 h-4 text-secondary dark:text-secondary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
@@ -118,7 +118,7 @@
           @endif
 
           @if (!empty($items))
-            <ul class="space-y-4 text-left text-neutral-600 dark:text-gray-400 mb-8 md:mb-0 ms-4">
+            <ul class="space-y-4 text-left text-neutral-600 dark:text-gray-400 mb-2 md:mb-0 ms-4">
               @foreach ($items as $item)
                 <li class="flex items-center space-x-3 rtl:space-x-reverse">
                 <svg class="shrink-0 w-4 h-4 text-secondary dark:text-secondary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
