2822f10223fd6a838516c69c9ffe0b73504e2358
José Paulino <jose.paulino@freebalance.com>
2025-03-24 21:58:38 -0400

    Update

--- resources/views/template-reset-password.blade.php
+++ resources/views/template-reset-password.blade.php
@@ -45,7 +45,7 @@
                     @endphp
                     
                     @if ($key_error)
-                        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 my-5 text-white border-red-500">
+                        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 my-5 border-red-500">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
